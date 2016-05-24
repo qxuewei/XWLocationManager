0b0e7fa5173b3fbb100fdab54b9755399b19853f
YellowChang <carayfire-develop@123.local>
2016-05-24 12:37:23 +0800

    增加动态演示图

---
+++ XWLocationManger/XWLocationManager.h
@@ -0,0 +1,35 @@
+//
+//  XWLocationManger.h
+//  Spread
+//
+//  Created by 邱学伟 on 16/5/23.
+//  Copyright © 2016年 邱学伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "Singleton.h"
+#import <UIKit/UIKit.h>
+
+/**
+ *  定位block块
+ *
+ *  @param location  当前位置对象
+ *  @param placeMark 反地理编码对象
+ *  @param error     错误信息
+ */
+typedef void(^ResultLocationInfoBlock)(CLLocation *location, CLPlacemark *placeMark, NSString *error);
+
+@interface XWLocationManager : NSObject
+//单例对象
+single_interface(XWLocationManager)
+
+
+/**
+ *  直接通过代码块获取用户位置信息
+ *
+ *  @param block 定位block代码块
+ */
+-(void)getCurrentLocation:(ResultLocationInfoBlock)block onViewController:(UIViewController *)viewController;
+
+@end
